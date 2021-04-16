1388ac9733f331c5da552947b134fb9086ebd28b
Oleksii Oleksenko <alexo_o@ukr.net>
2021-04-16 10:41:26 +0200

    evaluation/2_detection: update for dynamic fuzzing

--- evaluation/2_detection_speed/run.sh
+++ evaluation/2_detection_speed/run.sh
@@ -11,7 +11,7 @@ if [ -z "${WORK_DIR}" ]; then
 fi
 SCRIPT=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
-NUM_TESTS=1000
+NUM_TESTS=10000
 
 timestamp=$(date '+%y-%m-%d-%H-%M')
 instructions='instruction_sets/x86/base.xml'
@@ -20,19 +20,41 @@ log="$WORK_DIR/$timestamp/experiment.log"
 mkdir -p "$WORK_DIR/$timestamp"
 touch $log
 
-cd "$REVIZOR_DIR" || exit
+function time_to_violation() {
+    pushd "$REVIZOR_DIR" > /dev/null
 
-for name in v1-ct-seq v1-ct-bpas v4-ct-seq v4-ct-cond mds-ct-seq mds-ct-bpas mds-ct-cond ; do
+    local inst=$1
+    local conf=$2
+    local max_rounds=$3
+    local inputs=$4
+    local full_log=$5
+    local result=$6
+    local name=$7
+
+    ./cli.py fuzz -s $inst -c $conf -n $max_rounds -i $inputs -v  > tmp.txt
+    cat tmp.txt >> $full_log
+    cat tmp.txt | awk '/Test Cases:/{c=$3} /Patterns:/{p=$2} /Duration:/{d=$2} END{printf "%s, %d, %d, %s\n", name, c, p, d}' name=$name >> $result
+
+    popd > /dev/null
+}
+
+for name in v4-ct-seq v1-ct-seq mds-ct-seq v1-ct-cond v4-ct-cond v1-ct-bpas mds-ct-bpas mds-ct-cond v1-arch-seq v4-ct-bpas ; do
     echo "--------------------------------------------------------------------"
     echo "Running $name" 2>&1 | tee -a "$log"
     exp_dir="$WORK_DIR/$timestamp/$name"
-    config="$SCRIPT_DIR/$name.yaml"
+    result="$WORK_DIR/$timestamp/result.txt"
+    conf="$WORK_DIR/$timestamp/conf.yaml"
+    cp "$SCRIPT_DIR/$name.yaml" $conf
+
+    cp "$SCRIPT_DIR/$name.yaml" $conf
+    echo "prng_entropy_bits: 2
+feedback_driven_generator: true" >> $conf
 
     mkdir -p "$exp_dir"
 
-    echo "./cli.py fuzz -s $instructions -n $NUM_TESTS -i 10000 -v --nonstop -w $exp_dir -c $config"
-    ./cli.py fuzz -s $instructions -n $NUM_TESTS -i 10000 -v --nonstop -w $exp_dir -c $config 2>&1 | tee -a $log
-#    ./cli.py fuzz -s $instructions -n $NUM_TESTS -i 10000 -v -w $exp_dir -c $config 2>&1 | tee -a $log
+    for i in $(seq 0 9); do
+        time_to_violation $instructions $conf $NUM_TESTS 250 $log $result "$name,$i"
+    done
 done
 
-cd - || exit
+# datamash -t, --headers  groupby 1  mean 5 < tmp.txt | awk 'FS="," //{print $1, $2 / 60}'
