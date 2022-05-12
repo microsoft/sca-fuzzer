ab4352d525ce5a623918c22fd9102fd3daddacd7
Oleksii Oleksenko <alexo_o@ukr.net>
2022-05-12 12:03:18 +0100

    tests/expreriments: update x86 instr category names

--- src/tests/evaluation/impact_of_configuration/run.sh
+++ src/tests/evaluation/impact_of_configuration/run.sh
@@ -7,7 +7,7 @@ MAX_ROUNDS=10000
 
 timestamp=$(date '+%y-%m-%d-%H-%M')
 revizor_src='../../../'
-instructions="$revizor_src/instruction_sets/x86/base.xml"
+instructions="$revizor_src/x86/isa_spec/base.json"
 
 # RESULTS_DIR="results"
 exp_dir="$RESULTS_DIR/$timestamp"
