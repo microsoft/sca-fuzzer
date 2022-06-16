e56af6b0d315a74d31f70870624ef104094c306e
Oleksii Oleksenko <alexo_o@ukr.net>
2022-06-16 11:30:01 +0100

    x86/generator: always use both pages, not only when testing faults

--- src/x86/x86_generator.py
+++ src/x86/x86_generator.py
@@ -439,14 +439,9 @@ class X86SandboxPass(Pass):
 
     def __init__(self):
         super().__init__()
-        if CONF.enable_assist_page:
-            input_memory_size = CONF.input_main_region_size + CONF.input_assist_region_size
-        else:
-            input_memory_size = CONF.input_main_region_size
+        input_memory_size = CONF.input_main_region_size + CONF.input_assist_region_size
         mask_size = int(math.log(input_memory_size, 2))
-        self.sandbox_address_mask = "0b" + \
-                                    "1" * (mask_size - CONF.memory_access_zeroed_bits) + \
-                                    "0" * CONF.memory_access_zeroed_bits
+        self.sandbox_address_mask = "0b" + "1" * mask_size
 
     def run_on_test_case(self, test_case: TestCase) -> None:
         for func in test_case.functions:
