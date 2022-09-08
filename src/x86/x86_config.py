ae76f389ed8f269309d2e14c0cdddc60a8547d47
Oleksii Oleksenko <alexo_o@ukr.net>
2022-09-08 09:48:12 +0100

    config: user interface for controlling tested fault types

--- src/x86/x86_config.py
+++ src/x86/x86_config.py
@@ -9,6 +9,7 @@ from typing import List
 # x86_option_values attribute MUST be the first attribute in the file
 x86_option_values = {
     'executor_mode': ['P+P', 'F+R', 'E+R'],
+    'permitted_faults': [],
 }
 
 x86_instruction_categories: List[str] = [
