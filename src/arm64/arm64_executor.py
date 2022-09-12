a96c86c01688a9ffa747dce9d5d1ac8e9fbba518
emanuele <emanuele.vannacci@gmail.com>
2022-09-12 17:08:18 +0100

    arm64/executor: dummy ARM executor

--- src/arm64/arm64_executor.py
+++ src/arm64/arm64_executor.py
@@ -4,3 +4,20 @@ File:
 Copyright (C) Microsoft Corporation
 SPDX-License-Identifier: MIT
 """
+from typing import List, Tuple
+from interfaces import Input, Executor, CombinedHTrace
+
+
+class ARMDummyExecutor(Executor):
+
+    def load_test_case(self, _):
+        pass
+
+    def trace_test_case(self, inputs: List[Input], _) -> List[CombinedHTrace]:
+        return [0 for _ in inputs]
+
+    def read_base_addresses(self) -> Tuple[int, int]:
+        return (0x10000, 0x0)
+
+    def get_last_feedback(self):
+        pass
