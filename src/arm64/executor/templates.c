3a6d9ffedde11e4350569074e599257f19cf174e
Oleksii Oleksenko <alexo_o@ukr.net>
2022-09-16 11:04:00 +0100

    arm64/executor: input loading and saving of callee-saved regs

--- src/arm64/executor/templates.c
+++ src/arm64/executor/templates.c
@@ -4,6 +4,11 @@
 // SPDX-License-Identifier: MIT
 
 // -----------------------------------------------------------------------------------------------
+// Note on registers.
+// Some of the registers are reserved for a specific purpose and should never be overwritten.
+// These include:
+//   * X15 - hardware trace
+//
 
 #include "main.h"
 #include <linux/string.h>
@@ -74,10 +79,57 @@ int load_template(size_t tc_size) {
 // Template building blocks
 // =================================================================================================
 inline void prologue(void) {
+    // As we don't use a compiler to track clobbering,
+    // we have to save the callee-saved regs
+    asm volatile("" \
+        "stp x16, x17, [sp, #-16]!\n"
+        "stp x18, x19, [sp, #-16]!\n"
+        "stp x20, x21, [sp, #-16]!\n"
+        "stp x22, x23, [sp, #-16]!\n"
+        "stp x24, x25, [sp, #-16]!\n"
+        "stp x26, x27, [sp, #-16]!\n"
+        "stp x28, x29, [sp, #-16]!\n"
+        "str x30, [sp, #-16]!\n"
+
+        // x30 <- input base address (stored in x0, the first argument of measurement_code)
+        "mov x30, x0\n"
+
+        // stored_rsp <- sp
+        // "str sp, [x30, #"xstr(RSP_OFFSET)"]\n"
+        "mov x0, sp\n"
+        "str x0, [x30, #"xstr(RSP_OFFSET)"]\n"
+    );
 }
 
 inline void epilogue(void) {
+    asm volatile("" \
+        // store the hardware trace (x15)
+        "str x15, [x30, #"xstr(MEASUREMENT_OFFSET)"]\n"
+
+        // rsp <- stored_rsp
+        "ldr x0, [x30, #"xstr(RSP_OFFSET)"]\n"
+        "mov sp, x0\n"
+
+        // restore registers
+        "ldr x30, [sp], #16\n"
+        "ldp x28, x29, [sp], #16\n"
+        "ldp x26, x27, [sp], #16\n"
+        "ldp x24, x25, [sp], #16\n"
+        "ldp x22, x23, [sp], #16\n"
+        "ldp x20, x21, [sp], #16\n"
+        "ldp x18, x19, [sp], #16\n"
+        "ldp x16, x17, [sp], #16\n"
+    );
 }
+
+#define SET_REGISTER_FROM_INPUT() asm volatile("" \
+    "add sp, x30, #"xstr(REG_INIT_OFFSET)"\n" \
+    "ldp x0, x1, [sp], #16\n" \
+    "ldp x2, x3, [sp], #16\n" \
+    "ldp x4, x5, [sp], #16\n" \
+    "ldp x6, x7, [sp], #16\n" \
+    "msr nzcv, x6\n" \
+    "mov sp, x7\n");
 
 // =================================================================================================
 // L1D Prime+Probe
@@ -101,6 +153,9 @@ void template_l1d_prime_probe(void) {
     prologue();
 
     PRIME();
+
+    // Initialize registers
+    SET_REGISTER_FROM_INPUT();
 
     // Execute the test case
     asm("\nisb\n"
