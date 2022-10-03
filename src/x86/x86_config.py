31aad36d152b814c0dac91443b8b586964f8c86b
Oleksii Oleksenko <alexo_o@ukr.net>
2022-10-03 12:52:22 +0100

    x86/config: remove outdated comments

--- src/x86/x86_config.py
+++ src/x86/x86_config.py
@@ -107,27 +107,4 @@ x86_register_blocklist: List[str] = [
     'DR0', 'DR1', 'DR2', 'DR3', 'DR4', 'DR5', 'DR6', 'DR7'
 ]  # yapf: disable
 
-
-# X86_TRAP_DE = 1              # 0, Divide-by-zero
-# X86_TRAP_DB = 1 << 1         # 1, Debug
-# X86_TRAP_NMI = 1 << 2        # 2, Non-maskable Interrupt
-# X86_TRAP_BP = 1 << 3         # 3, Breakpoint
-# X86_TRAP_OF = 1 << 4         # 4, Overflow
-# X86_TRAP_BR = 1 << 5         # 5, Bound Range Exceeded
-# X86_TRAP_UD = 1 << 6         # 6, Invalid Opcode
-# X86_TRAP_NM = 1 << 7         # 7, Device Not Available
-# X86_TRAP_DF = 1 << 8         # 8, Double Fault
-# X86_TRAP_OLD_MF = 1 << 9     # 9, Coprocessor Segment Overrun
-# X86_TRAP_TS = 1 << 10        # 10, Invalid TSS
-# X86_TRAP_NP = 1 << 11        # 11, Segment Not Present
-# X86_TRAP_SS = 1 << 12        # 12, Stack Segment Fault
-# X86_TRAP_GP = 1 << 13        # 13, General Protection Fault
-# X86_TRAP_PF = 1 << 14        # 14, Page Fault
-# X86_TRAP_SPURIOUS = 1 << 15  # 15, Spurious Interrupt
-# X86_TRAP_MF = 1 << 16        # 16, x87 Floating-Point Exception
-# X86_TRAP_AC = 1 << 17        # 17, Alignment Check
-# X86_TRAP_MC = 1 << 18        # 18, Machine Check
-# X86_TRAP_XF = 1 << 19        # 19, SIMD Floating-Point Exception
-# X86_TRAP_IRET = 1 << 32      # 32, IRET Exception
-
 x86_disable_div64: bool = True
