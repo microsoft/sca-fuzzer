fc3b77323eb53df94a9bc8867935b0ad1c14e25e
Flavien Solt <flavien.solt97@gmail.com>
2023-11-10 15:08:33 +0100

    cli: clarify import error message (#77)

--- revizor.py
+++ revizor.py
@@ -8,8 +8,13 @@ SPDX-License-Identifier: MIT
 
 try:
     from src.cli import main
-except ImportError:
-    from revizor.cli import main
+except ImportError as orig_import_error:
+    try:
+        from revizor.cli import main
+    except ImportError:
+        print("Unable to import main from src.cli.")
+        print(f"Issue during import of src.cli: `{orig_import_error}`")
+        exit(1)
 
 
 if __name__ == '__main__':
