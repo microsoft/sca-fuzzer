168e8c98e58d1245516bbc19e6fc7f4758dec051
Flavien Solt <flavien.solt97@gmail.com>
2023-11-10 15:08:33 +0100

    cli: [fix] clarify import error message (#77)

--- revizor.py
+++ revizor.py
@@ -6,7 +6,15 @@ Copyright (C) Microsoft Corporation
 SPDX-License-Identifier: MIT
 """
 
-from src.cli import main
+try:
+    from src.cli import main
+except ImportError as orig_import_error:
+    try:
+        from revizor.cli import main
+    except ImportError:
+        print("Unable to import main from src.cli.")
+        print(f"Issue during import of src.cli: `{orig_import_error}`")
+        exit(1)
 
 
 if __name__ == '__main__':
