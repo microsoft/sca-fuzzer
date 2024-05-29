b5572ed77f5bfda9df9b6dd61cc9c235289fd90d
Oleksii Oleksenko <3688171+OleksiiOleksenko@users.noreply.github.com>
2024-05-29 19:04:19 +0100

    model/x86: [feat] add modelling of SVM-specific features

--- src/interfaces.py
+++ src/interfaces.py
@@ -859,6 +859,13 @@ class InstructionSetAbstract(ABC):
         pass
 
 
+class CPUDesc(NamedTuple):
+    vendor: str
+    model: str
+    family: str
+    stepping: str
+
+
 class TargetDesc(ABC):
     register_sizes: Dict[str, int]
     registers: Dict[int, List[str]]
@@ -869,6 +876,7 @@ class TargetDesc(ABC):
     macro_specs: Dict[str, MacroSpec]
     pte_bits: Dict[str, Tuple[int, bool]]
     epte_bits: Dict[str, Tuple[int, bool]]
+    cpu_desc: CPUDesc
 
     @staticmethod
     @abstractmethod
