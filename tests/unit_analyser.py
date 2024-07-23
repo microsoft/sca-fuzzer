6a9564c7b962b39c5aebaea1dd837929b959fb58
Oleksii Oleksenko <3688171+OleksiiOleksenko@users.noreply.github.com>
2024-07-23 15:35:24 +0100

    analyser: [refact] get rid of _build_eq... interface"
    (cherry picked from commit 6d2c8f6510e267f6593a40b633f382aef60d2b23)

--- tests/unit_analyser.py
+++ tests/unit_analyser.py
@@ -3,7 +3,7 @@ Copyright (C) Microsoft Corporation
 SPDX-License-Identifier: MIT
 """
 import unittest
-from src.analyser import MergedBitmapAnalyser, SetAnalyser, MWUAnalyser, ChiSquaredAnalyser
+from src.analyser import MergedBitmapAnalyser, SetAnalyser, ChiSquaredAnalyser
 from src.interfaces import Input, HTrace
 from src.config import CONF
 
@@ -13,95 +13,47 @@ class AnalyserTest(unittest.TestCase):
     def test_merged_bitmap_analyser(self):
         analyser = MergedBitmapAnalyser()
         dummy_input = Input()
+        inputs = [dummy_input] * 4
+
         h1 = HTrace([0b1101, 0b1101])
         h2 = HTrace([0b1011, 0b1011])
         h3 = HTrace([0b1000, 0b1000])
         h4 = HTrace([0b1000, 0b1000])
         htraces = [h1, h2, h3, h4]
+        ctraces = [1, 1, 2, 2]
 
-        # basic collection of eq classes
-        clss = analyser._build_equivalence_classes([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 2)
-        self.assertEqual(clss[0].ctrace, 1)
-        self.assertEqual(clss[0].measurements[0].htrace.raw, [0b1101, 0b1101])
-        self.assertEqual(clss[1].ctrace, 2)
-        self.assertEqual(clss[1].measurements[0].htrace.raw, [0b1000, 0b1000])
-
-        # detection of violations
-        clss = analyser.filter_violations([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 1)
-
-        # filtering of ineffective inputs
-        clss = analyser._build_equivalence_classes([dummy_input] * 4, [1, 2, 2, 2], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 2)
+        violations = analyser.filter_violations(inputs, ctraces, htraces)
+        self.assertEqual(len(violations), 1)
+        self.assertEqual(violations[0].ctrace, 1)
 
     def test_set_analyser(self):
         analyser = SetAnalyser()
         dummy_input = Input()
+        inputs = [dummy_input] * 4
+
         h1 = HTrace([1, 2, 2, 1])
         h2 = HTrace([1, 3, 3, 1])
         h3 = HTrace([1, 1, 1, 1])
         h4 = HTrace([1, 1, 1, 1])
         htraces = [h1, h2, h3, h4]
+        ctraces = [1, 1, 2, 2]
 
-        # basic collection of eq classes
-        clss = analyser._build_equivalence_classes([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 2)
-        self.assertEqual(clss[0].ctrace, 1)
-        self.assertEqual(clss[0].measurements[0].htrace.raw, [1, 2, 2, 1])
-        self.assertEqual(clss[1].ctrace, 2)
-        self.assertEqual(clss[1].measurements[0].htrace.raw, [1, 1, 1, 1])
-
-        # detection of violations
-        clss = analyser.filter_violations([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 1)
-
-        # filtering of ineffective inputs
-        clss = analyser._build_equivalence_classes([dummy_input] * 4, [1, 2, 2, 2], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 2)
-
-    @unittest.skip("MWUAnalyser is not implemented yet")
-    def test_mwu_analyser(self):
-        analyser = MWUAnalyser()
-        dummy_input = Input()
-        h1 = [1, 1, 1, 1, 1]
-        h2 = [300000, 300000, 300000, 300000, 300000]
-        h3 = [1, 1]
-        h4 = [1, 1]
-        htraces = [HTrace(h1), HTrace(h2), HTrace(h3), HTrace(h4)]
-
-        # basic collection of eq classes
-        clss = analyser._build_equivalence_classes([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 2)
-        self.assertEqual(clss[0].ctrace, 1)
-        self.assertEqual(clss[0].measurements[0].htrace.raw, h1)
-        self.assertEqual(clss[1].ctrace, 2)
-        self.assertEqual(clss[1].measurements[0].htrace.raw, h3)
-
-        # detection of violations
-        clss = analyser.filter_violations([dummy_input] * 4, [1, 1, 2, 2], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 1)
+        violations = analyser.filter_violations(inputs, ctraces, htraces)
+        self.assertEqual(len(violations), 1)
+        self.assertEqual(violations[0].ctrace, 1)
 
     def test_chi2_analyser(self):
         analyser = ChiSquaredAnalyser()
         dummy_input = Input()
+        inputs = [dummy_input] * 4
+
         h1 = [1] * CONF.executor_sample_sizes[0]
         h2 = [2] * CONF.executor_sample_sizes[0]
         h2[0] = 1
         h2[1] = 1
-        htraces = [HTrace(h1), HTrace(h2)]
+        htraces = [HTrace(h1), HTrace(h2), HTrace(h2), HTrace(h2)]
+        ctraces = [1, 1, 2, 2]
 
-        # basic collection of eq classes
-        clss = analyser._build_equivalence_classes([dummy_input] * 2, [1, 1], htraces)
-        self.assertEqual(len(clss), 1)
-        self.assertEqual(clss[0].ctrace, 1)
-        self.assertEqual(clss[0].measurements[0].htrace.raw, h1)
-
-        # detection of violations
-        clss = analyser.filter_violations([dummy_input] * 2, [1, 1], htraces)
-        self.assertEqual(len(clss), 1)
+        violations = analyser.filter_violations(inputs, ctraces, htraces)
+        self.assertEqual(len(violations), 1)
+        self.assertEqual(violations[0].ctrace, 1)
