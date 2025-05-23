790a61cec8acd28528dee3191d23304615863fa3
Oleksii Oleksenko <3688171+OleksiiOleksenko@users.noreply.github.com>
2025-05-23 16:13:42 +0100

    consfuzz: [feat] initial, very rough implementation

---
+++ consfuzz/fuzzer.py
@@ -0,0 +1,75 @@
+"""
+File: Implementation of the high-level fuzzing logic for the software leakage fuzzer.
+
+Copyright (C) Microsoft Corporation
+SPDX-License-Identifier: MIT
+"""
+from __future__ import annotations
+from typing import TYPE_CHECKING, List
+
+import pathlib
+
+from .pub_gen import PubGen
+from .sec_gen import SecGen
+from .tracer import Tracer
+from .reporter import Reporter
+
+if TYPE_CHECKING:
+    from .config import Config
+
+
+class FuzzerCore:
+    """
+    Class responsible for orchestrating the fuzzing process.
+    """
+    _config: Config
+    _working_dir: str
+
+    def __init__(self, config: Config) -> None:
+        self._config = config
+
+    def generate_public_inputs(self, cmd: List[str], target_cov: int, timeout_s: int) -> int:
+        """
+        Fuzzing Stage 1:
+            Generate public inputs with PubGen.
+
+        :param cmd: Command to run the target binary, with placeholders for public (@@)
+                    and private (@#) inputs
+        :param target_cov: Target coverage to achieve
+        :param timeout_s: Timeout for the fuzzing process
+        :return: 0 if the target coverage or timeout is reached, 1 if error occurs
+        """
+        pathlib.Path(self._config.stage1_wd).mkdir(parents=True, exist_ok=True)
+        pub_gen = PubGen(self._config)
+        return pub_gen.generate(cmd, target_cov, timeout_s)
+
+    def stage2(self, cmd: List[str], num_sec_inputs: int) -> int:
+        """
+        Fuzzing Stage 2:
+            Generate private inputs, and collect contract traces for each public-private input pair.
+
+        :param cmd: Command to run the target binary, with placeholders for public (@@)
+                    and private (@#) inputs
+        :param num_sec_inputs: Number of secret (private) inputs to generate for each public input
+        :return: 0 if successful, 1 if error occurs
+        """
+        pathlib.Path(self._config.stage2_wd).mkdir(parents=True, exist_ok=True)
+        sec_gen = SecGen(self._config)
+        tracer = Tracer(self._config)
+
+        sec_gen.generate(cmd, num_sec_inputs)
+        return tracer.collect_traces(cmd)
+
+    def report(self, target_binary: str) -> int:
+        """
+        Fuzzing Stage 3:
+            Analyze the target binary for software leakage and generate a report.
+
+        :param target_binary: Path to the target binary
+        :return: 0 if successful, 1 if error occurs
+        """
+        pathlib.Path(self._config.stage3_wd).mkdir(parents=True, exist_ok=True)
+        reporter = Reporter(self._config)
+        reporter.analyze()
+        reporter.generate_report(target_binary)
+        return 0
