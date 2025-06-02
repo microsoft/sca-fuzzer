895090f72483866bf5d84b1aef2cf22ad4fb0ed1
Oleksii Oleksenko <3688171+OleksiiOleksenko@users.noreply.github.com>
2025-06-02 12:53:36 +0100

    cfz/log: [feat] add a logging module to the fuzzer

--- consfuzz/logger.py
+++ consfuzz/logger.py
@@ -34,3 +34,25 @@ class ProgressBar:
             flush=True)
         if self._current >= self._total:
             print("")  # Print a newline when the progress bar is complete
+
+
+class Logger:
+    """
+    Logger class for managing logging operations.
+    """
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+    def critical(self, message: str) -> None:
+        """Log a critical message with the logger's name."""
+        print(f"[CRITICAL] {self._name}: {message}")
+        raise SystemExit(1)
+
+    def error(self, message: str) -> None:
+        """Log an error message with the logger's name."""
+        print(f"[ERROR] {self._name}: {message}")
+
+    def info(self, message: str) -> None:
+        """Log an informational message with the logger's name."""
+        print(f"[INFO] {self._name}: {message}")
